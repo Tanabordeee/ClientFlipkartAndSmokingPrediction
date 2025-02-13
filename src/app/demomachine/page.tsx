9bdde0cadebf1b38c4fc46ccd65dc20018a275a5
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-02-13 12:14:48 +0700

    change to min height

--- src/app/demomachine/page.tsx
+++ src/app/demomachine/page.tsx
@@ -1,8 +1,10 @@
 import SvmPredictForm from "./SvmPredictForm"
 import RfPredictionForm from "./RfPredictionForm"
+import CompareModel from "./Compare"
 export default function demomachine(){
     return(
-        <div className="flex flex-col  items-center h-screen">
+        <div className="flex flex-col  items-center min-h-screen">
+        <CompareModel></CompareModel>
         <SvmPredictForm></SvmPredictForm>
         <RfPredictionForm></RfPredictionForm>
         </div>
