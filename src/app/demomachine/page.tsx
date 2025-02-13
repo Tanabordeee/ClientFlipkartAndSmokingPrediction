2fe931bdedc32de9b8a9bf12141b2e86260d6e8c
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-02-13 11:40:21 +0700

    add RF form

--- src/app/demomachine/page.tsx
+++ src/app/demomachine/page.tsx
@@ -1,8 +1,10 @@
 import SvmPredictForm from "./SvmPredictForm"
+import RfPredictionForm from "./RfPredictionForm"
 export default function demomachine(){
     return(
         <div className="flex flex-col  items-center h-screen">
         <SvmPredictForm></SvmPredictForm>
+        <RfPredictionForm></RfPredictionForm>
         </div>
     )
 }
