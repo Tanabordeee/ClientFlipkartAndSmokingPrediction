85631b4ba34e846e14ca68a0c5e826c681b53ec2
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2025-02-13 11:27:10 +0700

    add svm Form

--- src/app/demomachine/page.tsx
+++ src/app/demomachine/page.tsx
@@ -1,9 +1,8 @@
 import SvmPredictForm from "./SvmPredictForm"
 export default function demomachine(){
     return(
-        <>
-
+        <div className="flex flex-col  items-center h-screen">
         <SvmPredictForm></SvmPredictForm>
-        </>
+        </div>
     )
 }
